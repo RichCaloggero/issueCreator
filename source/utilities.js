68e18af409f0e1a768241e8929a45eb39c34c2ee
Rich Caloggero <rjc@mit.edu>
2017-01-26 17:01:18 -0500

    * building transformers
    - added to utilities.js
    - ugh!

--- source/utilities.js
+++ source/utilities.js
@@ -36,25 +36,6 @@ function objectToOrderedPairs (object, keys) {
 } // objectToOrderedPairs 
 
 
-/// tests
-
-/*var $table;
-var issues = [{
-	title: "title1",
-	code: "code1",
-	guideline: "1.1.1"
-}, {
-	title: "title2",
-	code: "code2",
-	guideline: "2.1.1"
-}];
-//debug ("issues: ", issues.length);
-
-$table = createIssueTable (issues, ["guideline", "code", "title"]);
-//debug ("table: ", $("tr", $table).length);
-$table.appendTo ("body");
-*/
-
 function toCsv (list, keys) {
 var result = "";
 result += JSON.stringify(keys).slice (1,-1) + "\n";
@@ -67,5 +48,69 @@ result += JSON.stringify(values).slice (1,-1) + "\n";
 
 return result;
 } // toCsv
+
+var transformers = {
+display: {
+codeExample: marked
+}, formData: {
+_default: "val",
+codeExample: "html",
+screenshot: "html"
+}
+}; // transformers
+
+function getFormData ($fields, transformer = transformers.form) {
+return fromPairs(
+$fields.get().map (($field)
+=> [getFieldName($field), $field[method(getFieldName($field), transformer)]])
+); // fromPairs
+} // getFormData
+
+function setFormData ($fields, object, transformer = transformers.form) {
+$fields.each (function () {
+var $field = $(this);
+var name = getFieldName($field);
+$field[method(name, transformer)] (object[name]);
+}); // each
+} // setFormData
+
+
+function transformObject (object, transformer = transformers.display) {
+var identity = function (x) {return x;};
+
+return _.mapValues (object, function (value, key) {
+switch (typeof(transformer[key])) {
+case "undefined": return identity (value);
+case "function": return transform[key] (value);
+
+default: alert ("invalid transformer -- should never happen!");
+} // switch
+return false;
+}); // mapValues
+} // transformObject
+
+
+
+function method (object, key, transformer) {
+var methodName = "";
+if (! transformer[key]) {
+if (transformer._default) {
+methodName = transformer._default;
+} else {
+alert ("invalid transform, no default set");
+throw new Error ("invalid transform, no default set");
+} // if
+
+} else if (transformer[key] instanceof Function) {
+methodName = transformer[key] (object, key);
+
+} else if (typeof(transformer[key] === "string")) {
+methodName = transformer[key];
+} // if
+
+
+return methodName;
+} // method
+
 
 //alert ("utilities.js loaded");
